b02bfce1159d2aa2d8fc8db8aa17619ac632231e
AlanGarcia50 <agarcia153@student.butte.edu>
2025-09-25 17:33:39 +0000

    I did function practice

--- Hello_World.py
+++ Hello_World.py
@@ -20,3 +20,8 @@ b = arr.array('l', [1, 2, 3, 4, 5])
 print(b.pop())
 
 print("hello world")
+
+def f(x): # define a function using "def" <name>(<input list>)
+    return (x + 1) # must return some value 
+
+print(f(2), f(4), f(4), f(6))
